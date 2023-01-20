9b339039d3b02c09f5aac559a5d02116dd282c58
Eric-Barajas <barajaseric505@gmail.com>
2023-01-19 20:56:29 -0800

    init commit

---
+++ week2/w2d4OrderedMultisetUnion.js
@@ -0,0 +1,79 @@
+/* 
+  Union Sorted Arrays
+  Efficiently combine two already-sorted multiset arrays
+  into a new sorted array containing the multiset union.
+  Unions by default will take the set of dupes
+  that has the highest occurrences from one array.
+  Venn Diagram Visualization (top) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+*/
+
+const nums1A = [1, 2, 2, 2, 7];
+const nums1B = [2, 2, 6, 6, 7];
+const expected1 = [1, 2, 2, 2, 6, 6, 7];
+
+const nums2A = [1, 1, 2, 2, 2, 3, 7, 10, 20, 30];
+const nums2B = [2, 6, 6, 7];
+const expected2 = [1, 1, 2, 2, 2, 3, 6, 6, 7, 10, 20, 30];
+
+const nums3A = [];
+const nums3B = [2, 2, 3, 3, 3];
+const expected3 = [2, 2, 3, 3, 3];
+
+const nums4A = [2, 2, 3, 3, 3];
+const nums4B = [];
+const expected4 = [2, 2, 3, 3, 3];
+
+const nums5A = [];
+const nums5B = [];
+const expected5 = [];
+/* 
+  Explanation: Every int from each multiset is included in the result, for dupes, like 2, include it 3 times,
+  because it occurs 3 times at most in ONE multiset
+*/
+
+/**
+ * Combines two already sorted multiset arrays into an ordered multiset union
+ * Venn Diagram Visualization (top):
+ * @see https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} sortedA Both sets are sorted multisets
+ *    (contain dupes).
+ * @param {Array<number>} sortedB
+ * @returns {Array<number>} An ordered multiset union of the given sets.
+ *    The return should include dupes, but the amount of dupes for each int
+ *    should be based on the max amount that dupe appears from one set,
+ *    not the combined amount from both sets.
+ */
+function orderedMultisetUnion(sortedA, sortedB) {
+    let newArray = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < sortedA.length || j < sortedB.length) {
+
+        if (i === sortedA.length) {
+            newArray.push(sortedB[j++]);
+            continue;
+        } else if (j === sortedB.length) {
+            newArray.push(sortedA[i++]);
+            continue;
+        }
+
+        if (sortedA[i] === sortedB[j]) {
+            newArray.push(sortedA[i++]);
+            j++
+            continue;
+        } else if (sortedA[i] < sortedB[j]) {
+            newArray.push(sortedA[i++]);
+        } else {
+            newArray.push(sortedB[j++])
+        }
+
+
+    }
+    return newArray;
+}
+
+console.log(orderedMultisetUnion(nums1A, nums1B))
+console.log(orderedMultisetUnion(nums2A, nums2B))
